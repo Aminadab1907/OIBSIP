1b72836b41cb608d3a049c0aefd36ca2837297c1
Aminadab1907 <aminadabzerai@gmail.com>
2024-02-01 00:53:54 +0300

    BMI Classfication and Output added to main.py

--- BMI_Calculator/main.py
+++ BMI_Calculator/main.py
@@ -10,11 +10,42 @@ height = float(input("Enter your height in meters:  "))
 # Define a function to calculate BMI from User Input
 def bmiCalculator(height,weight):
     bmi = weight / (height * height)
-    return (bmi)
+    return round(bmi,2)
 
 
 # print the bmi on the screen
-print(bmiCalculator(height,weight))
+bmi_result =  bmiCalculator(height,weight)
+print("Your BMI is ",bmi_result)
+
+
+
+""" BMI CLASSIFICATION TABLE
+BMI	                Weight Status
+--------------      ---------------
+Below 18.5	        Underweight
+18.5 – 24.9	        Healthy Weight
+25.0 – 29.9	        Overweight
+30.0 and Above	    Obesity
+"""
+
+#A fucntion to classify BMI result into the above categories
+
+def bmiClassify(result):
+    if (result >= 30):
+        return 'Obesity'
+    elif (result < 30 and result >=25):
+        return 'Overweight'
+    elif (result < 35 and result >=18.5):
+        return 'Healthy Weight'
+    else:
+        return 'Underweight'
+
+
+print ("Your BMI Classification is ",bmiClassify(bmi_result))
+
+
+
+
 
 
 
