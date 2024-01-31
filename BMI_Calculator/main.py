d14ce3aea232fe2cd95ec90cae599f1fe034cbc6
Aminadab1907 <aminadabzerai@gmail.com>
2024-02-01 00:28:44 +0300

    Basic BMI Calculator, NO Obesity Classification

--- BMI_Calculator/main.py
+++ BMI_Calculator/main.py
@@ -1,1 +1,23 @@
-print("Hello, World")
+'''This is a script that calculates the Body Mass Index of an individual when
+he/she adds their height and weight and the appropriate obesity level is given
+back to the user.'''
+
+
+# Ask the user to input their height and weight
+weight = float(input("Enter your weight in Kilograms:   "))
+height = float(input("Enter your height in meters:  "))
+
+# Define a function to calculate BMI from User Input
+def bmiCalculator(height,weight):
+    bmi = weight / (height * height)
+    return (bmi)
+
+
+# print the bmi on the screen
+print(bmiCalculator(height,weight))
+
+
+
+
+
+
